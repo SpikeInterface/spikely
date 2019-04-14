36b5dbf78359b4a2736fabd51617d86d77b8fcdf
Roger Hurwitz <rogerhurwitz@gmail.com>
2019-04-14 12:03:25 -0700

    Added feature not implemented code to Queue button

--- op_view.py
+++ op_view.py
@@ -8,21 +8,33 @@ and queueing the active pipeline.
 
 import sys
 import PyQt5.QtWidgets as qw
+import PyQt5.QtGui as qg
+
 from pi_model import SpikePipeline  # The model for this controller
 
 
 class OperatePipelineView(qw.QGroupBox):
-    """GroupBox of widgets capable of controlling active pipeline.
+    """GroupBox of widgets capable of operating the active pipeline.
 
-    No public methods other than constructor.  All other activites
-    of object are triggered by user interaction with sub widgets.
+    Invokes methods on the active pipeline. No public methods other than
+    constructor.  All other activites of object are triggered by user
+    interaction with sub widgets.
     """
 
-    def __init__(self, spike_pipe):
+    def __init__(self, active_pipe):
         """Initialize parent, set object members, construct UI."""
-        super().__init__("Operate Pipeline")  # Labels group box parent
-        self.spike_pipe = spike_pipe
+        super().__init__("Operate Pipeline")  # Applies group box label
+        self._pipe = active_pipe
         self._init_ui()
+
+    def _feature_not_implemented(self):
+        msg_box = qw.QMessageBox(self.parent())
+        msg_box.setIcon(qw.QMessageBox.Information)
+        msg_box.setText("Feature not implemented")
+        msg_box.setInformativeText(
+            "... not to say there aren't plans to do so.")
+        msg_box.setStandardButtons(qw.QMessageBox.Ok)
+        msg_box.exec()
 
     def _init_ui(self):
         hbox = qw.QHBoxLayout()
@@ -30,13 +42,13 @@ class OperatePipelineView(qw.QGroupBox):
 
         # Pipeline operation commands
         run_btn = qw.QPushButton("Run")
-        run_btn.clicked.connect(self.spike_pipe.run)
+        run_btn.clicked.connect(self._pipe.run)
         hbox.addWidget(run_btn)
 
         clear_btn = qw.QPushButton("Clear")
-        clear_btn.clicked.connect(self.spike_pipe.clear)
+        clear_btn.clicked.connect(self._pipe.clear)
         hbox.addWidget(clear_btn)
 
         queue_btn = qw.QPushButton("Queue")
-        queue_btn.clicked.connect(lambda: print("Queue not implemented."))
+        queue_btn.clicked.connect(self._feature_not_implemented)
         hbox.addWidget(queue_btn)
