beef7746f74ed9ff88954fe86e1ab129bb7037db
Roger Hurwitz <rogerhurwitz@gmail.com>
2019-06-30 22:51:12 -0700

    Another setup.py fix

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ version = {}
 with open('spikely/version.py') as fp:
     exec(fp.read(), version)
 
+with open('README.md', 'r') as fp:
+    long_description = fp.read()
+
 
 setup(
     name='spikely',
@@ -11,12 +14,14 @@ setup(
     packages=['spikely'],
     install_requires=[
         'PyQt5>=5.12.3',
-        'spikeextractors',
-        'spiketoolkit'
+        'spikeextractors>=0.5.3',
+        'spiketoolkit>=0.3.5'
     ],
     author='Roger Hurwitz',
     author_email='rogerhurwitz@gmail.com',
-    description='An app wrapper around spikeextractors and spiketoolkit',
+    description='A GUI around spikeextractors and spiketoolkit',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/SpikeInterface/spikely',
     entry_points={
         'console_scripts': [
