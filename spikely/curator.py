a98fbcc4fb34feb1d829b600a42ca45d044f0690
colehurwitz <colehurwitz@gmail.com>
2019-07-05 15:27:21 +0100

    Export to phy working correctly after curation

--- spikely/curator.py
+++ spikely/curator.py
@@ -1,6 +1,7 @@
 from spike_element import SpikeElement
 import spikeextractors as se
 from pathlib import Path
+import spiketoolkit as st
 import os
 
 
@@ -15,6 +16,7 @@ class Curator(SpikeElement):
         params_dict = {}
         params_dict['sorting'] = input_payload[0]
         output_folder_path = input_payload[1]
+        recording = input_payload[2]
 
         params = self._params
         for param in params:
@@ -26,10 +28,9 @@ class Curator(SpikeElement):
         sorting = self._interface_class(**params_dict)
         if(next_element is None):
             curated_output_folder = output_folder_path/'curated_phy_results'
-            #curated_output_folder = Path(curated_output_folder_path).absolute()
             if not curated_output_folder.is_dir():
                 os.makedirs(str(curated_output_folder))
             print("Saving curated results....")
-            se.PhySortingExtractor.write_sorting(sorting, curated_output_folder)
+            st.postprocessing.export_to_phy(recording, sorting, curated_output_folder)#, grouping_property='group')
             print("Done!")
         return sorting, output_folder_path
