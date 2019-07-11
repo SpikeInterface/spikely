b0e3d384a5ec5f4c02e0806810c8e4289988dd95
colehurwitz <colehurwitz@gmail.com>
2019-07-11 19:59:31 +0100

    probe file handling complete

--- spikely/sorter.py
+++ spikely/sorter.py
@@ -11,16 +11,14 @@ class Sorter(SpikeElement):
     def run(self, input_payload, next_element):
         base_sorter_param_dict = {}
         sub_sorter_param_dict = {}
-        base_sorter_param_dict['recording'] = input_payload
-        output_folder = None
-        
+        base_sorter_param_dict['recording'] = input_payload        
         params = self._params
         for param in params:
             param_name = param['name']
             param_value = param['value']
             if 'base_param' in param:
                 base_sorter_param_dict[param_name] = param_value
-                if(param_name == 'output_folder'):
+                if param_name == 'output_folder':
                     output_folder_string = param_value
             else:
                 sub_sorter_param_dict[param_name] = param_value
@@ -28,4 +26,7 @@ class Sorter(SpikeElement):
         sorter = self._interface_class(**base_sorter_param_dict)
         sorter.set_params(**sub_sorter_param_dict)
         sorter.run()
+        if output_folder_string is None:
+            output_folder_string = 'tmp_' + sorter.sorter_name
+            
         return sorter.get_result_list(), output_folder_string, input_payload
