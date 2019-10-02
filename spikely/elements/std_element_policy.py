c800d87e2fb9e08b748166249d7ab4c770bfb129
Roger Hurwitz <rogerhurwitz@gmail.com>
2019-10-02 12:26:31 -0700

    Moved element cls display name lookup to ElementPolicy class

--- spikely/elements/std_element_policy.py
+++ spikely/elements/std_element_policy.py
@@ -9,14 +9,21 @@ from . import sorting_exporter as sp_soe
 class StdElementPolicy(sp_elp.ElementPolicy):
 
     def __init__(self):
-        a_required_cls_list = [
+        required_cls_list = [
             sp_ree.RecordingExtractor, sp_sor.Sorter]
 
-        a_cls_order_dict = {
+        cls_order_dict = {
             sp_ree.RecordingExtractor: 0, sp_pre.Preprocessor: 1,
             sp_sor.Sorter: 2, sp_cur.Curator: 3, sp_soe.SortingExporter: 4}
 
-        super().__init__(a_required_cls_list, a_cls_order_dict)
+        cls_display_name_dict = {
+            sp_ree.RecordingExtractor: 'Recording Extractor',
+            sp_pre.Preprocessor: 'Preprocessor',
+            sp_sor.Sorter: 'Sorter', sp_cur.Curator: 'Curator',
+            sp_soe.SortingExporter: 'Sorting Extractor'}
+
+        super().__init__(required_cls_list, cls_order_dict,
+                         cls_display_name_dict)
 
     def is_cls_available(self, cls):
         return cls in [
