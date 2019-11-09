9cd9b5f85f3acc2d4c39bad7de9281d84d1f32af
Roger Hurwitz <rogerhurwitz@gmail.com>
2019-11-09 11:26:25 -0800

    Updated load order of import to avoid circular references

--- spikely/__init__.py
+++ spikely/__init__.py
@@ -1,9 +1,9 @@
 # Make version id available at a package level
+from .elements.spike_element import SpikeElement            # noqa: F401
+from .elements.std_element_policy import StdElementPolicy   # noqa: F401
 from .pipeline_model import PipelineModel                   # noqa: F401
 from .pipeline_view import PipelineView                     # noqa: F401
 from .parameter_view import ParameterView                   # noqa: F401
 from .parameter_model import ParameterModel                 # noqa: F401
 from .operation_view import OperationView                   # noqa: F401
-from .elements.spike_element import SpikeElement            # noqa: F401
-from .elements.std_element_policy import StdElementPolicy   # noqa: F401
 from .version import __version__                            # noqa: F401
