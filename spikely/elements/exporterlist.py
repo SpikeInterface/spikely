87dfd56fe22acd8fb1ab5ad6be3bc6d93f2e872d
lwty <luwangtianyu@gmail.com>
2020-06-01 14:41:39 +0800

    AttributeError: module 'spikeextractors' has no attribute 'sorting_exporter_dict'
    'sorting_exporter_dict' should be changed to 'sorting_extractor_dict'

--- spikely/elements/exporterlist.py
+++ spikely/elements/exporterlist.py
@@ -5,7 +5,7 @@ exporters_list = []
 exporters_list.extend(se.extractorlist.writable_sorting_extractor_list)
 exporters_list.append(PhyExporter)
 
-se.sorting_exporter_dict
+se.sorting_extractor_dict
 
-sorting_exporter_dict = se.sorting_exporter_dict
+sorting_exporter_dict = se.sorting_extractor_dict
 sorting_exporter_dict[PhyExporter.exporter_name] = PhyExporter
