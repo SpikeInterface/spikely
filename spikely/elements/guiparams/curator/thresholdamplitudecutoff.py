9785e9321d6690995e0f4cb1275622d98e762a9b
colehurwitz <colehurwitz@gmail.com>
2020-06-06 16:48:29 -0700

    new ones

--- spikely/elements/guiparams/curator/thresholdamplitudecutoff.py
+++ spikely/elements/guiparams/curator/thresholdamplitudecutoff.py
@@ -12,21 +12,15 @@ gui_params = [
                   If 'greater', will threshold any metric greater than the given threshold. \
                   If 'greater_or_equal', will threshold any metric greater than or equal to the given threshold.",
     },
-    {
-        "name": "num_channels_to_compare",
-        "type": "int",
-        "value": 13,
-        "default": 13,
-        "title": "The number of channels to be used for the PC extraction and comparison.",
-    },
-    {
-        "name": "max_spikes_per_cluster",
-        "type": "int",
-        "value": 500,
-        "default": 500,
-        "title": "Max spikes to be used from each unit.",
-    },
     #kwargs
+    {
+        "name": "method",
+        "type": "str",
+        "value": "absolute",
+        "default": "absolute",
+        "title": "If 'absolute' (default), amplitudes are absolute amplitudes in uV are returned. \
+                  If 'relative', amplitudes are returned as ratios between waveform amplitudes and template amplitudes.",
+    },
     {
         "name": "method",
         "type": "str",
@@ -79,64 +73,6 @@ gui_params = [
         "title": "Low-pass frequency for optional filter (default 6000 Hz).",
     },
     {
-        "name": "grouping_property",
-        "type": "str",
-        "value": None,
-        "default": None,
-        "title": "Property to group channels. E.g. if the recording extractor has the 'group' property and \
-                 'grouping_property' is 'group', then waveforms are computed group-wise.",
-    },
-    {
-        "name": "ms_before",
-        "type": "float",
-        "value": 3.,
-        "default": 3.,
-        "title": "Time period in ms to cut waveforms before the spike events.",
-    },
-    {
-        "name": "ms_after",
-        "type": "float",
-        "value": 3.,
-        "default": 3.,
-        "title": "Time period in ms to cut waveforms after the spike events.",
-    },
-    {
-        "name": "dtype",
-        "type": "dtype",
-        "value": None,
-        "default": None,
-        "title": "The numpy dtype of the waveforms.",
-    },
-    {
-        "name": "compute_property_from_recording",
-        "type": "bool",
-        "value": False,
-        "default": False,
-        "title": "If True and 'grouping_property' is given, the property of each unit is assigned as the corresponding \
-                  property of the recording extractor channel on which the average waveform is the largest.",
-    },
-    {
-        "name": "max_channels_per_waveforms",
-        "type": "int",
-        "value": None,
-        "default": None,
-        "title": " Maximum channels per waveforms to return. If None, all channels are returned.",
-    },
-    {
-        "name": "n_jobs",
-        "type": "int",
-        "value": None,
-        "default": None,
-        "title": "Number of parallel jobs (default None).",
-    },
-    {
-        "name": "memmap",
-        "type": "bool",
-        "value": True,
-        "default": True,
-        "title": "If True, waveforms are saved as memmap object (recommended for long recordings with many channels).",
-    },
-    {
         "name": "save_property_or_features",
         "type": "bool",
         "value": True,
@@ -156,19 +92,5 @@ gui_params = [
         "value": 300,
         "default": 300,
         "title": "The maximum number of spikes to extract per unit.",
-    },
-    {
-        "name": "seed",
-        "type": "int",
-        "value": 0,
-        "default": 0,
-        "title": "Random seed for reproducibility.",
-    },
-    {
-        "name": "verbose",
-        "type": "bool",
-        "value": False,
-        "default": False,
-        "title": "If True, output from SpikeInterface element is verbose when run.",
     },
 ]
