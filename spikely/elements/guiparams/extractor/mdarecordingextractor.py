86fc9272c042eef6c14a41c48591e1b9ee35b0c0
colehurwitz <colehurwitz@gmail.com>
2020-06-06 16:39:00 -0700

    mda gui params

---
+++ spikely/elements/guiparams/extractor/mdarecordingextractor.py
@@ -0,0 +1,28 @@
+gui_params = [
+    {
+        "name": "folder_path",
+        "type": "folder",
+        "title": "Path to folder.",
+    },
+    {
+        "name": "raw_fname",
+        "type": "file",
+        "value": "raw.mda",
+        "default": "raw.mda",
+        "title": "File name of raw file (default raw.mda).",
+    },
+    {
+        "name": "params_fname",
+        "type": "file",
+        "value": "params.json",
+        "default": "params.json",
+        "title": "File name of params file (default params.json).",
+    },
+    {
+        "name": "geom_fname",
+        "type": "file",
+        "value": "geom.csv",
+        "default": "geom.csv",
+        "title": "File name of geom file (default geom.csv).",
+    },
+]
