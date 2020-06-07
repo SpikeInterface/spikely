9ad13207f27af1e05df0fbb437e2536ad232d186
Roger Hurwitz <rogerhurwitz@gmail.com>
2020-06-07 13:07:52 -0700

    Integrated gui_params into spikely package.

---
+++ spikely/elements/guiparams/preprocessor/clip.py
@@ -0,0 +1,16 @@
+gui_params = [
+    {
+        "name": "a_min",
+        "type": "float",
+        "value": None,
+        "default": None,
+        "title": "Minimum value. If `None`, clipping is not performed on lower interval edge.",
+    },
+    {
+        "name": "a_max",
+        "type": "float",
+        "value": None,
+        "default": None,
+        "title": "Maximum value. If `None`, clipping is not performed on upper interval edge.",
+    },
+]
