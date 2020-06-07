a9002bef46bef57f8cdfde34b45f5a1361efeaed
Roger Hurwitz <rogerhurwitz@gmail.com>
2020-06-07 12:56:08 -0700

    Removed debug print statement.

---
+++ spikely/elements/guiparams/preprocessor/blanksaturation.py
@@ -0,0 +1,17 @@
+gui_params = [
+    {
+        "name": "threshold",
+        "type": "float",
+        "value": None,
+        "default": None,
+        "title": "Threshold value (in absolute units) for saturation artifacts. "
+                 "If None, the threshold will be determined from the 0.1 signal percentile.",
+    },
+    {
+        "name": "seed",
+        "type": "int",
+        "value": 0,
+        "default": 0,
+        "title": "Random seed for reproducibility.",
+    },
+]
