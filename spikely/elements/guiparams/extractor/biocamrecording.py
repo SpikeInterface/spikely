065485f9b4a552c1c60f5eb0ee65772875da634c
Roger Hurwitz <rogerhurwitz@gmail.com>
2020-06-14 16:39:14 -0700

    Added gui_params file.

---
+++ spikely/elements/guiparams/extractor/biocamrecording.py
@@ -0,0 +1,21 @@
+gui_params = [
+    {
+        "name": "file_path",
+        "type": "file",
+        "title": "Path to file (.h5 or .hdf5)."
+    },
+    {
+        "name": "verbose",
+        "type": "bool",
+        "value": False,
+        "default": False,
+        "title": "If true, provide verbose messages.",
+    },
+    {
+        "name": "mea_pitch",
+        "type": "int",
+        "value": 42,
+        "default": 42,
+        "title": "The pitch of the MEA.",
+    },
+]
