621052062d9cd56559f195a72135f5c0c9ba4ddb
Roger Hurwitz <rogerhurwitz@gmail.com>
2020-06-14 20:41:42 -0700

    Added gui_params files.

---
+++ spikely/elements/guiparams/extractor/spikeglxrecordingextractor.py
@@ -0,0 +1,21 @@
+gui_params = [
+    {
+        "name": "file_path",
+        "type": "file",
+        "title": "Path to neuropixels ap or lf file",
+    },
+    {
+        "name": "x_pitch",
+        "type": "int",
+        "value": 21,
+        "default": 21,
+        "title": "x_pitch for Neuropixels probe (default 21)",
+    },
+    {
+        "name": "y_pitch",
+        "type": "int",
+        "value": 20,
+        "default": 20,
+        "title": "y_pitch for Neuropixels probe (default 20)",
+    },
+]
