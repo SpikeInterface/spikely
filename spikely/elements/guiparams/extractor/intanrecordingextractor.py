1b06495507697ca71b290fff73d58debbec2245f
Roger Hurwitz <rogerhurwitz@gmail.com>
2020-06-14 19:16:48 -0700

    Added gui_params file.

---
+++ spikely/elements/guiparams/extractor/intanrecordingextractor.py
@@ -0,0 +1,14 @@
+gui_params = [
+    {
+        "name": "file_path",
+        "type": "file",
+        "title": "Path to file (.rhs or .rhd)"
+    },
+    {
+        "name": "verbose",
+        "type": "bool",
+        "value": False,
+        "default": False,
+        "title": "If true, use detailed messages."
+    },
+]
---
+++ spikely/elements/guiparams/extractor/mcsh5recordingextractor.py
@@ -0,0 +1,21 @@
+gui_params = [
+    {
+        "name": "file_path",
+        "type": "file",
+        "title": "Path to file (.h5 or .hdf5)"
+    },
+    {
+        "name": "stream_id",
+        "type": "int",
+        "value": 0,
+        "default": 0,
+        "title": "ID of stream that will be loaded"
+    },
+    {
+        "name": "verbose",
+        "type": "bool",
+        "value": False,
+        "default": False,
+        "title": "If true, use verbose messages.",
+    },
+]
