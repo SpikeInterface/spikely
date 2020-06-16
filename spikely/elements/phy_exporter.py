23aecb332bb62312f81c1384d096572d545ccfc3
colehurwitz <colehurwitz@gmail.com>
2020-06-15 19:32:54 -0700

    phy exporter complete?

--- spikely/elements/phy_exporter.py
+++ spikely/elements/phy_exporter.py
@@ -7,14 +7,9 @@ class PhyExporter:
     mode = 'folder'
 
     @staticmethod
-    def write_sorting(recording, sorting, save_path, n_comp=3, electrode_dimensions=None,
-                      grouping_property=None, ms_before=1., ms_after=2., dtype=None, amp_method='absolute', amp_peak='both',
-                      amp_frames_before=3, amp_frames_after=3, max_spikes_for_pca=100000,
-                      recompute_info=True, save_features_props=False, write_waveforms=False, verbose=False,
-                      seed=0):
+    def write_sorting(recording, sorting, save_path, compute_pc_features,
+                      compute_amplitudes, max_channels_per_template, **kwargs):
 
-        export_to_phy(recording, sorting, save_path, n_comp, electrode_dimensions,
-                      grouping_property, ms_before, ms_after, dtype, amp_method, amp_peak,
-                      amp_frames_before, amp_frames_after, max_spikes_for_pca,
-                      recompute_info, save_features_props, write_waveforms, verbose,
-                      seed)
+        export_to_phy(recording, sorting, save_path, compute_pc_features=compute_pc_features,
+                      compute_amplitudes=compute_amplitudes, 
+                      max_channels_per_template=max_channels_per_template, **kwargs)
